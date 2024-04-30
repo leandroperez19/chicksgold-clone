7b9827760c72873de3e773589ca5e73e0829b50f
Leandro Perez <Leandro_perez03@outlook.com>
2024-04-30 01:39:29 -0400

    Fix: Route solving try 1

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         ),
     },
     {
-        path: "/items/",
+        path: "/items",
         element: (
             <DefaultLayout>
                 <ItemsPage />
